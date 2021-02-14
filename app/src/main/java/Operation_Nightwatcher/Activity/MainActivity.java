199fa4cc8d0809dfbca5463f5ae3c87e5f5baf1e
Seafoam <lu.brandon2020@gmail.com>
2021-02-14 15:21:00 -0800

    made packages

--- app/src/main/java/Operation_Nightwatcher/Activity/MainActivity.java
+++ app/src/main/java/Operation_Nightwatcher/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.td.OperationNightwatcher;
+package Operation_Nightwatcher.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.td.OperationNightwatcher.R;
 
 public class MainActivity extends AppCompatActivity {
 
