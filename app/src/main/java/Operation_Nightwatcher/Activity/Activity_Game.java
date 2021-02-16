5b57148a5d7133494d8275e8eb448eb5018e9b1f
Justin <justinhwang92@gmail.com>
2021-02-15 22:06:09 -0500

    Add basic override functions

--- app/src/main/java/Operation_Nightwatcher/Activity/Activity_Game.java
+++ app/src/main/java/Operation_Nightwatcher/Activity/Activity_Game.java
@@ -12,4 +12,15 @@ public class Activity_Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+    @Override
+    protected void onRestart(){ super.onRestart(); }
 }
