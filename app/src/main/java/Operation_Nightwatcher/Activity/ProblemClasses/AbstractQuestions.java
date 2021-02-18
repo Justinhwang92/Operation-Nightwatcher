5ba366a72f66888128884a5dcc6f90e7dfa7ce0b
Bhavesh Joshi <bjoshi912@gmail.com>
2021-02-17 23:38:12 -0800

    created question classes.

---
+++ app/src/main/java/Operation_Nightwatcher/Activity/ProblemClasses/AbstractQuestions.java
@@ -0,0 +1,11 @@
+package Operation_Nightwatcher.Activity.ProblemClasses;
+
+public abstract class AbstractQuestions {
+
+    String myQuestion;
+    String myAnswer;
+
+    public abstract String getQuestion();
+
+    public abstract String getAnswer();
+}
