a62ac01ef0437c12ea284c6fff2cd89fdf0dadb7
Seafoam <lu.brandon2020@gmail.com>
2021-02-28 13:33:10 -0800

    fixed android manifest, calculator class (in progress), calculator prototype layout

--- app/src/main/java/Operation_Nightwatcher/Activity/Activity_Menu.java
+++ app/src/main/java/Operation_Nightwatcher/Activity/Activity_Menu.java
@@ -11,25 +11,29 @@ import android.widget.TextView;
 
 import com.td.OperationNightwatcher.R;
 
+import Operation_Nightwatcher.Game.Calculator;
+
 public class Activity_Menu extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_menu);
 
+        setContentView(R.layout.activity_menu);
         // Initialize textViews and buttons
-        TextView tittle = findViewById(R.id.title);
+        TextView title = findViewById(R.id.title);
         Button playButton = findViewById(R.id.playButton);
         Button instructionButton = findViewById(R.id.instructionButton);
-        Button tdgamesButton = findViewById(R.id.tdgamesButton);
+        Button tdGamesButton = findViewById(R.id.tdgamesButton);
 
         // Attach the click listener to the buttons
         playButton.setOnClickListener(this);
         instructionButton.setOnClickListener(this);
-        tdgamesButton.setOnClickListener(this);
+        tdGamesButton.setOnClickListener(this);
+
     }
+
 
     @Override
     public void onClick(View view) {
