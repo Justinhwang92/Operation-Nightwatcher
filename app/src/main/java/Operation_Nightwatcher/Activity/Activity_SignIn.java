9375c4eced5afe8ad9fa457b06c91411fe2a24bc
Justin <justinhwang92@gmail.com>
2021-03-04 21:06:20 -0500

    Add upload picture function

--- app/src/main/java/Operation_Nightwatcher/Activity/Activity_SignIn.java
+++ app/src/main/java/Operation_Nightwatcher/Activity/Activity_SignIn.java
@@ -1,18 +1,41 @@
 package Operation_Nightwatcher.Activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.td.OperationNightwatcher.R;
 
-public class Activity_SignIn extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Activity_SignIn extends AppCompatActivity {
+    public static final int TAKE_PIC_REQUEST_CODE = 0;
+    public static final int CHOOSE_PIC_REQUEST_CODE = 1;
+    public static final int MEDIA_TYPE_IMAGE = 2;
+
+    private Uri mMediaUri;
+
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,17 +45,109 @@ public class Activity_SignIn extends AppCompatActivity implements View.OnClickLi
         // Initialize textView and button
         EditText userName = findViewById(R.id.userName);
         Button okButton = findViewById(R.id.okButton);
+        ImageView img_profile = findViewById(R.id.img_profile);
+        ImageView img_plus = findViewById(R.id.img_plus);
 
         // Attach the click listener to the buttons
-        okButton.setOnClickListener(this);
+        okButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_SignIn.this, Activity_Game.class);
+                startActivity(intent);
+            }
+        });
+
+        img_plus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "Change Pic Pressed", Toast.LENGTH_SHORT).show();
+
+                //show dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(Activity_SignIn.this);
+                builder.setTitle("Upload or Take a photo");
+                builder.setPositiveButton("Upload", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //upload image
+                        Intent choosePictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                        choosePictureIntent.setType("image/*");
+                        startActivityForResult(choosePictureIntent, CHOOSE_PIC_REQUEST_CODE);
+                    }
+                });
+
+                builder.setNegativeButton("Take Photo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //take photo
+                        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+                        if (mMediaUri == null) {
+                            //display error
+                            Toast.makeText(getApplicationContext(), "Sorry there was an error! Try again.", Toast.LENGTH_LONG).show();
+                        } else {
+                            takePicture.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                            startActivityForResult(takePicture, TAKE_PIC_REQUEST_CODE);
+                        }
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                try{
+                    final InputStream imageStream = getContentResolver().openInputStream(mMediaUri); ;
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    img_profile.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        Intent i = new Intent(this, Activity_Game.class);
-        startActivity(i);
-        finish();
+
+    private Uri getOutputMediaFileUri(int mediaTypeImage) {
+
+        if (isExternalStorageAvailable()) {
+            //get the URI
+            //get external storage dir
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "UPLOADIMAGES");
+            //create subdirectore if it does not exist
+            if (!mediaStorageDir.exists()) {
+                //create dir
+                if (!mediaStorageDir.mkdirs()) {
+
+                    return null;
+                }
+            }
+            //create a file name
+            //create file
+            File mediaFile = null;
+            Date now = new Date();
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(now);
+
+            String path = mediaStorageDir.getPath() + File.separator;
+            if (mediaTypeImage == MEDIA_TYPE_IMAGE) {
+                mediaFile = new File(path + "IMG_" + timestamp + ".jpg");
+            }
+            //return file uri
+            Log.d("UPLOADIMAGE", "FILE: " + Uri.fromFile(mediaFile));
+
+            return Uri.fromFile(mediaFile);
+        } else {
+
+            return null;
+        }
     }
+
+    //check if external storage is mounted. helper method
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     @Override
     protected void onStop(){
