874d165c6f6347618e7057344356777ad0211b6d
Bhavesh Joshi <56456911+bjoshi99@users.noreply.github.com>
2021-03-27 15:12:20 -0700

    add app icon, update name of the game, change bulb icon location, make timer working consistent

--- app/src/main/java/Operation_Nightwatcher/Activity/Activity_Menu.java
+++ app/src/main/java/Operation_Nightwatcher/Activity/Activity_Menu.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
